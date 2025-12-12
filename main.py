a3cf8c8c97176c978cd9bc78ebc802029884ee3c
chris <c.gebhardt@columbia.edu>
2025-12-11 21:06:02 -0500

    note removed in main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Complete your game here
-
 # * Concept
 #   You play as the monster. Coins are "packages" you need to collect and deliver to the door. 
 #   Robots act as patrolling obstacles that try to catch you. 
